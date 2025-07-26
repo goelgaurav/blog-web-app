43c34293e47afb1141541b2969f6a8392adbc7f7
Gaurav Goel <gauravvgoel12@gmail.com>
2025-07-26 02:55:52 -0400

    chore: scaffold Domain,Persistence and Application projects, add to solution and wire references, included axios

--- blog-backend/src/Blog.Api/Program.cs
+++ blog-backend/src/Blog.Api/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
