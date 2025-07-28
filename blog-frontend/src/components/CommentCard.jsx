2b563685bb96c50af5f722f503e87b531f92328b
Gaurav Goel <gauravvgoel12@gmail.com>
2025-07-28 12:56:50 -0400

    fixed error, dates showing as invalid on comments: fixed postedAt property on commentResponse from api

--- blog-frontend/src/components/CommentCard.jsx
+++ blog-frontend/src/components/CommentCard.jsx
@@ -1,20 +1,20 @@
 export default function CommentCard({ comment }) {
 
     let date = '';
-    if (comment.createdAt) {
+    if (comment.postedAt) {
         const d = new Date(comment.postedAt);
         date = isNaN(d.getTime())
-        ? ''
-        : d.toLocaleString();
+            ? ''
+            : d.toLocaleString();
     }
 
-  return (
-    <div className="border p-4 rounded mb-2 bg-gray-50">
-      <p className="text-sm text-gray-700">{comment.content}</p>
-      <div className="text-xs text-gray-500 mt-1">
-        By <span className="font-medium">{comment.author || 'Anonymous'}</span> at{' '}
-        {date && ` — ${date}`}
-      </div>
-    </div>
-  );
+    return (
+        <div className="border p-4 rounded mb-2 bg-gray-50">
+            <p className="text-sm text-gray-700">{comment.content}</p>
+            <div className="text-xs text-gray-500 mt-1">
+                By <span className="font-medium">{comment.author || 'Anonymous'}</span> at{' '}
+                {date && ` — ${date}`}
+            </div>
+        </div>
+    );
 }
