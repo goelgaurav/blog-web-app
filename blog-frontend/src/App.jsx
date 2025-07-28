4ff5d294eb0918d3ad7ca9492ab0b59109413eb8
Gaurav Goel <gauravvgoel12@gmail.com>
2025-07-28 05:04:55 -0400

    configured App.jsx routes & nav, added global ErrorBoundary

--- blog-frontend/src/App.jsx
+++ blog-frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary';
 import BlogPostList from './pages/BlogPosts/BlogPostList';
 function App() {
 
   return (
-      <div className="App">
-        <BlogPostList />
-      </div>
+    <BrowserRouter>
+      <nav className="p-4 bg-gray-800 text-white">
+        <Link to="/" className="mr-4">Posts</Link>
+      </nav>
+      <ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<BlogPostList />}/>
+      </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
   );
 }
 
